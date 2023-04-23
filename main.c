54528486855e3c8b8259e48118e3e0ebce9c406e
Mark <marknjoropge@gmail.com>
2023-04-23 04:06:53 -0700

    Document and reformat all current files

--- main.c
+++ main.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
+/**
+ * main - main entry point for interpreter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char cmd[MAX_LINE];
+	char *args[MAX_LINE / 2 + 1];
+	char *full_path;
 
+	while (1)
+	{
+		if (read_input(cmd))
+		{
+			break;
+		}
+		if (strcmp(cmd, "exit") == 0)
+			break;
 
+	tokenize_input(cmd, args);
+	full_path = search_path(args[0]);
+	if (full_path == NULL)
+	{
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		continue;
+	}
+	execute_command(args, full_path);
+	free(full_path);
+	}
 
-
-
-
-
-
-int main(void) {
-    char cmd[MAX_LINE];
-    char *args[MAX_LINE/2 + 1];
-    char *full_path;
-    
-    while (1) {
-       if (read_input(cmd)) {
-        break;
-        }
-        if (strcmp(cmd, "exit") == 0) {
-            break;
-        }
-        tokenize_input(cmd, args);
-        full_path = search_path(args[0]);
-        if (full_path == NULL) {
-            fprintf(stderr, "%s: command not found\n", args[0]);
-            continue;
-        }
-        execute_command(args, full_path);
-        free(full_path);
-    }
-    
-    return 0;
+	return (0);
 }
--- tokenizeinput.c
+++ tokenizeinput.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
+/**
+ * tokenize_input - parse input on interpreter
+ * @cmd: original command from terminal
+ * @args: array of strings for individual command tokens
+ *
+ * Return: nothing
+ */
+void tokenize_input(char *cmd, char **args)
+{
+	char *token;
+	int i = 0;
 
-
-void tokenize_input(char *cmd, char **args) {
-    char *token;
-    int i = 0;
-    token = strtok(cmd, " ");
-    while (token != NULL) {
-        args[i] = token;
-        token = strtok(NULL, " ");
-        i++;
-    }
-    args[i] = NULL;
+	token = strtok(cmd, " ");
+	while (token != NULL)
+	{
+		args[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
 }
