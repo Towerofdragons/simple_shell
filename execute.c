6a6d28135ee54ff84630dbcab9c44ed2ed9afe18
michaelitindi <57675705+michaelitindi@users.noreply.github.com>
2023-04-27 02:30:25 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,42 +1,41 @@
 #include "main.h"
 
-int execute(data_of_program *d)
+int execute_cmd(program_data *d)
 {
-	int retval = 0, status;
-	pid_t pidd;
+    int retval = 0, status;
+    pid_t pidd;
 
-	
-	retval = builtins_list(d);
-	if (retval != -1)
-		return (retval);
-	
-	retval = find_program(d);
-	if (retval)
-	{
-		return (retval);
-	}
-	else
-	{
-		pidd = fork(); 
-		if (pidd == -1)
-		{ 
-			perror(d->command_name);
-			exit(EXIT_FAILURE);
-		}
-		if (pidd == 0)
-		{
-			retval = execve(d->tokens[0], d->tokens, d->env);
-			if (retval == -1) 
-				perror(d->command_name), exit(EXIT_FAILURE);
-		}
-		else
-		{
-			wait(&status);
-			if (WIFEXITED(status))
-				errno = WEXITSTATUS(status);
-			else if (WIFSIGNALED(status))
-				errno = 128 + WTERMSIG(status);
-		}
-	}
-	return (0);
+    retval = check_builtins(d);
+    if (retval != -1)
+        return (retval);
+
+    retval = search_program(d);
+    if (retval)
+    {
+        return (retval);
+    }
+    else
+    {
+        pidd = fork();
+        if (pidd == -1)
+        {
+            perror(d->cmd_name);
+            exit(EXIT_FAILURE);
+        }
+        if (pidd == 0)
+        {
+            retval = execve(d->tokens[0], d->tokens, d->env_vars);
+            if (retval == -1)
+                perror(d->cmd_name), exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+            if (WIFEXITED(status))
+                errno = WEXITSTATUS(status);
+            else if (WIFSIGNALED(status))
+                errno = 128 + WTERMSIG(status);
+        }
+    }
+    return (0);
 }
