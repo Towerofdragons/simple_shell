074621f59fa007870f1919eca8ed0ad8a8cd06d8
michaelitindi <57675705+michaelitindi@users.noreply.github.com>
2023-04-27 02:40:34 +0300

    Update helper.c

--- helper.c
+++ helper.c
@@ -1,43 +1,41 @@
 #include "main.h"
 
-void free_recurrent_data(data_of_program *d)
+void free_recurrent_data(program_data *d)
 {
-	if (d->tokens)
-		free_array_of_pointers(d->tokens);
-	if (d->input_line)
-		free(d->input_line);
-	if (d->command_name)
-		free(d->command_name);
+    if (d->tokens)
+        free_pointer_array(d->tokens);
+    if (d->input_str)
+        free(d->input_str);
+    if (d->cmd_name)
+        free(d->cmd_name);
 
-	d->input_line = NULL;
-	d->command_name = NULL;
-	d->tokens = NULL;
+    d->input_str = NULL;
+    d->cmd_name = NULL;
+    d->tokens = NULL;
 }
 
-
-void free_all_data(data_of_program *d)
+void free_all_data(program_data *d)
 {
-	if (d->file_descriptor != 0)
-	{
-		if (close(d->file_descriptor))
-			perror(d->program_name);
-	}
-	free_recurrent_data(d);
-	free_array_of_pointers(d->env);
-	free_array_of_pointers(d->alias_list);
+    if (d->file_desc != 0)
+    {
+        if (close(d->file_desc))
+            perror(d->prog_name);
+    }
+    free_recurrent_data(d);
+    free_pointer_array(d->env_vars);
+    free_pointer_array(d->alias_arr);
 }
 
-
-void free_array_of_pointers(char **arr)
+void free_pointer_array(char **arr)
 {
-	int i;
+    int i;
 
-	if (arr != NULL)
-	{
-		for (i = 0; arr[i]; i++)
-			free(arr[i]);
+    if (arr != NULL)
+    {
+        for (i = 0; arr[i]; i++)
+            free(arr[i]);
 
-		free(arr);
-		arr = NULL;
-	}
+        free(arr);
+        arr = NULL;
+    }
 }
