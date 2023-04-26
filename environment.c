471820908f9e047ebe520d0d33fa2f0f2e2fd5ae
Rodney Njau <70792076+Towerofdragons@users.noreply.github.com>
2023-04-27 00:38:26 +0300

    Update data

--- environment.c
+++ environment.c
@@ -1,39 +1,33 @@
-#include "shell.h"
+#include "main.h"
 
-/**
- * builtin_env - shows the environment where the shell runs
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
- */
 int builtin_env(data_of_program *data)
 {
 	int i;
 	char cpname[50] = {'\0'};
 	char *var_copy = NULL;
 
-	/* if not arguments */
+	
 	if (data->tokens[1] == NULL)
 		print_environ(data);
 	else
 	{
 		for (i = 0; data->tokens[1][i]; i++)
-		{/* checks if exists a char = */
+		{
 			if (data->tokens[1][i] == '=')
-			{/* checks if exists a var with the same name and change its value*/
-			/* temporally */
+			{
 				var_copy = str_duplicate(env_get_key(cpname, data));
 				if (var_copy != NULL)
 					env_set_key(cpname, data->tokens[1] + i + 1, data);
 
-				/* print the environ */
+				
 				print_environ(data);
 				if (env_get_key(cpname, data) == NULL)
-				{/* print the variable if it does not exist in the environ */
+				{
 					_print(data->tokens[1]);
 					_print("\n");
 				}
 				else
-				{/* returns the old value of the var*/
+				{
 					env_set_key(cpname, var_copy, data);
 					free(var_copy);
 				}
@@ -48,45 +42,37 @@ int builtin_env(data_of_program *data)
 	return (0);
 }
 
-/**
- * builtin_set_env - ..
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
- */
-int builtin_set_env(data_of_program *data)
+
+int builtin_set_env(data_of_program *d)
 {
-	/* validate args */
-	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+	
+	if (d->tokens[1] == NULL || d->tokens[2] == NULL)
 		return (0);
-	if (data->tokens[3] != NULL)
+	if (d->tokens[3] != NULL)
 	{
 		errno = E2BIG;
-		perror(data->command_name);
+		perror(d->command_name);
 		return (5);
 	}
 
-	env_set_key(data->tokens[1], data->tokens[2], data);
+	env_set_key(d->tokens[1], d->tokens[2], d);
 
 	return (0);
 }
 
-/**
- * builtin_unset_env - ..
- * @data: struct for the program's data'
- * Return: ..
- */
-int builtin_unset_env(data_of_program *data)
+
+int builtin_unset_env(data_of_program *d)
 {
-	/* validate args */
-	if (data->tokens[1] == NULL)
+	
+	if (d->tokens[1] == NULL)
 		return (0);
-	if (data->tokens[2] != NULL)
+	if (d->tokens[2] != NULL)
 	{
 		errno = E2BIG;
-		perror(data->command_name);
+		perror(d->command_name);
 		return (5);
 	}
-	env_remove_key(data->tokens[1], data);
+	env_remove_key(d->tokens[1], d);
 
 	return (0);
 }
