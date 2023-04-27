d5e902e70808f26d9d2a794740f9a40af754e0fc
Rodney Njau <70792076+Towerofdragons@users.noreply.github.com>
2023-04-27 03:46:26 +0300

    Remove str_concat dup

--- helper_strings.c
+++ helper_strings.c
@@ -72,55 +72,6 @@ int str_compare(char *string1, char *string2, int number)
     }
 }
 
-char *str_concat(char *string1, char *string2)
-{
-    char *result;
-    int length1 = 0, length2 = 0;
-
-    if (string1 == NULL)
-        string1 = "";
-    length1 = str_length(string1);
-
-    if (string2 == NULL)
-        string2 = "";
-    length2 = str_length(string2);
-
-    result = malloc(sizeof(char) * (length1 + length2 + 1));
-    if (result == NULL)
-    {
-        errno = ENOMEM;
-        perror("Error");
-        return (NULL);
-    }
-
-    /* copy of string1 */
-    for (length1 = 0; string1[length1] != '\0'; length1++)
-        result[length1] = string1[length1];
-
-	if (string1 == NULL || string2 == NULL)
-		return (0);
-
-	if (number == 0) /* infinite longitud */
-	{
-		if (str_length(string1) != str_length(string2))
-			return (0);
-		for (iterator = 0; string1[iterator]; iterator++)
-		{
-			if (string1[iterator] != string2[iterator])
-				return (0);
-		}
-		return (1);
-	}
-	else /* if there is a number of chars to be compared */
-	{
-		for (iterator = 0; iterator < number ; iterator++)
-		{
-			if (string1[iterator] != string2[iterator])
-			return (0);
-		}
-		return (1);
-	}
-}
 
 char *str_concat(char *string1, char *string2)
 {
